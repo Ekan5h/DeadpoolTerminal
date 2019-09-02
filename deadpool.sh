769f999dd8e295df5a632e3226d05f334b476d7a
Ekansh Mahendru <ekanshmahendru@gmail.com>
2019-09-02 23:13:47 +0530

    Add the bash script

---
+++ deadpool.sh
@@ -0,0 +1,29 @@
+RED='\033[0;31m'
+NC='\033[0m'
+echo -e "
+			${RED}           XXXXXXXXXX
+				XXXXXXXXXXXXXXXX
+			     XXXXXXXXXXXXXXXXXXXXXX
+			   XXXX       XXXX       XXXX
+			  XXXX        XXXX        XXXX
+			 XXXX         XXXX         XXXX
+			XXXX          XXXX          XXXX
+			XXX           XXXX           XXX
+			XXX  ${NC}***${RED}      XXXX      ${NC}***${RED}  XXX
+			XXX  ${NC}******* ${RED} XXXX ${NC} *******${RED}  XXX
+			XXXX   ${NC}******${RED} XXXX ${NC}******${RED}   XXXX
+			XXXX    ${NC}****${RED}  XXXX  ${NC}****${RED}    XXXX
+			 XXX          XXXX          XXX
+			  XXXX        XXXX        XXXX
+			    XXXXX     XXXX     XXXXX
+			       XXXXXXXXXXXXXXXXXXX
+				   XXXXXXXXXXXX
+"
+declare exp=("			  \"I'm touching myself tonight." "		  \"Never underestimate the stupidity of idiots." "				  \"Chimichangas" "		    \"If you can read this, you're too close." "				   \"Ew, people")
+RANDOM=$$$(date +%s)
+selectedexpression=${exp[$RANDOM % ${#exp[@]} ]}
+echo -e "
+${NC}$selectedexpression\"
+						${RED}-Deadpool
+"
+
